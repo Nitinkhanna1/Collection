267653f5ad186499ad18eb67cf6a09408b675e53
Nitin khanna <khannanitin105@gmail.com>
2019-10-19 00:57:53 -0700

    first push ready

---
+++ src/Collection.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Collection {
+    public static void main(String[] args) {
+        ArrayList obj=new ArrayList();
+        obj.add(1);
+        System.out.println(obj);
+    }
+}
